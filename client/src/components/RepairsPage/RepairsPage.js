f47437aa9663ba25f41a21782e6b67880bf5c888
Radek Novacek <rad.n@centrum.cz>
2017-09-20 23:53:14 +0200

    client: implement fetching repairs

--- client/src/components/RepairsPage/RepairsPage.js
+++ client/src/components/RepairsPage/RepairsPage.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { graphql, gql } from 'react-apollo';
 import { Input, Menu } from 'semantic-ui-react';
 
 import RepairsList from './RepairsList';
 
-const RepairsPage = ({ }) => {
-    const repairs = [];
+const RepairsPage = ({ allRepairsQuery, mine }) => {
+    if (allRepairsQuery && allRepairsQuery.loading) {
+        return <div>Loading</div>;
+    }
+    if (allRepairsQuery && allRepairsQuery.error) {
+        console.error(allRepairsQuery.error);
+        return <div>Error</div>;
+    }
+
+    const repairs = mine ?
+        allRepairsQuery.allRepairs.filter(
+            r => (r.assignedTo && r.assignedTo.id === allRepairsQuery.me.id),
+        )
+        :
+        allRepairsQuery.allRepairs;
 
     return (
         <div>
@@ -26,7 +40,36 @@ const RepairsPage = ({ }) => {
 };
 
 RepairsPage.propTypes = {
-
+    allRepairsQuery: PropTypes.shape({
+        loading: PropTypes.bool.isRequired,
+        error: PropTypes.object,
+        allRepairs: PropTypes.array,
+        me: PropTypes.shape({
+            id: PropTypes.string.isRequired,
+        }),
+    }).isRequired,
+    mine: PropTypes.bool.isRequired,
 };
 
-export default RepairsPage;
+RepairsPage.defaultProps = {
+    mine: false,
+};
+
+const ALL_REPAIRS_QUERY = gql`
+    query AllRepairsQuery {
+        allRepairs {
+            id
+            title
+            scheduledTo
+            assignedTo {
+                name
+            }
+            completedAt
+        }
+        me {
+            id
+        }
+    }
+`;
+
+export default graphql(ALL_REPAIRS_QUERY, { name: 'allRepairsQuery' })(RepairsPage);
