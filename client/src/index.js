23270a5f047bea0ea4920a238bcc16f2955e170e
Radek Novacek <rad.n@centrum.cz>
2017-09-20 23:46:24 +0200

    client: implement authentication

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo';
+import { Router } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import 'semantic-ui-css/semantic.min.css';
 
 import './site.css';
 import registerServiceWorker from './registerServiceWorker';
-import App from './App';
+import App from './components/App';
+
+const history = createHistory();
+
 
 const networkInterface = createNetworkInterface({
-    uri: '__SIMPLE_API_ENDPOINT__',
+    uri: '/graphql',
 });
+
+networkInterface.use([
+    {
+        applyMiddleware({ options }, next) {
+            const token = localStorage.getItem('token');
+            if (token) {
+                if (!options.headers) {
+                    options.headers = {};
+                }
+                options.headers.authorization = `Bearer ${token}`;
+            }
+            next();
+        },
+    },
+]);
+
+networkInterface.useAfter([
+    {
+        applyAfterware({ response }, next) {
+            if (response.status === 401) {
+                history.push(`/login?next=${history.location.pathname}`);
+            }
+            next();
+        },
+    },
+]);
+
 
 const client = new ApolloClient({
     networkInterface,
@@ -17,7 +49,9 @@ const client = new ApolloClient({
 
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <App />
+        <Router history={history}>
+            <App />
+        </Router>
     </ApolloProvider>,
     document.getElementById('root'),
 );
